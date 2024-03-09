ed1e67be75a958400b4ad5a0a86382d7dfdad810
ddkwork <2762713521@qq.com>
2024-03-09 11:10:40 +0800

    up style

--- ollamaGui.go
+++ ollamaGui.go
@@ -68,8 +68,8 @@ func main() {
 	})
 	//gi.NewTextField(downframe).SetText("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx").Style(func(s *styles.Style) { s.SetTextWrap(true) })
 	gi.NewTextField(downframe).SetText("Multiline textfield with a  long initial text").Style(func(s *styles.Style) {
-		s.Min.X.Set(200, 200) //todo height not working
-		s.SetTextWrap(true)
+		s.Max.X.Set(0, 0) //todo height not working
+		s.Max.Y.Set(0, 0)
 	})
 	gi.NewButton(downframe).SetText("send").Style(func(s *styles.Style) {
 		s.Min.Set(units.Dp(33))
